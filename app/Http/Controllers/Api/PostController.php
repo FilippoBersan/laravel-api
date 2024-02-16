2500ccbf85a5038f2d3f2725e200998905ee7b14
FilippoBersan <filippobersan99@gmail.com>
2024-02-16 16:53:47 +0100

    feat: paginazione

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,10 +12,12 @@ class PostController extends Controller
     public function index()
     {
 
-        $posts = Post::all();
+        // $posts = Post::all();
+
+        $posts = Post::paginate(9);
         return response()->json([
             'status'=> true,
-            'data ' => $posts
+            'results' => $posts
 
         ]);
     }
